6f817d997a8f0443ae2618105029973b2623312a
Sarthak Nayak <zarthak23n@gmail.com>
2025-08-16 02:01:59 +0530

    stage 3

--- app/main.py
+++ app/main.py
@@ -18,6 +18,17 @@ def main():
                 break  # Client closed connection
             connection.sendall(pong.encode())
 
+def main():
+    print("Logs from your program will appear here!")
+
+    server_socket = socket.create_server(("localhost", 6379))
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    while True:
+        connection, _ = server_socket.accept()
+        # Start a new thread for each client
+        threading.Thread(target=handle_client, args=(connection,), daemon=True).start()
+
 
 if __name__ == "__main__":
     main()

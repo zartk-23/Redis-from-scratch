498b59337257ae21fcef460a96b2ed1608679f06
Sarthak Nayak <zarthak23n@gmail.com>
2025-08-16 01:49:50 +0530

    stage 2

--- app/main.py
+++ app/main.py
@@ -4,18 +4,17 @@ import socket  # noqa: F401
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
     # Uncomment this to pass the first stage
     pong= "+PONG\r\n"
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 6379)
-    
-    connection, _ = server_socket.accept()
-    
-    with connection:
-        connection.recv(1024)
-        connection.send(pong.encode())
+    server_socket = socket.create_server(("localhost", 6379), reuse_addr=True)
+    while True:
+        connection, _ = server_socket.accept()
+        with connection:
+            data = connection.recv(1024)
+            if data:
+                connection.sendall(pong.encode())
 
 
 if __name__ == "__main__":

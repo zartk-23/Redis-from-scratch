935d24012b28487b719d2041a8709641bf14d607
Sarthak Nayak <zarthak23n@gmail.com>
2025-08-16 01:46:30 +0530

    Implement Stage 1: Respond with PONG

--- app/main.py
+++ app/main.py
@@ -6,11 +6,16 @@ def main():
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
+    pong= "+PONG\r\n"
+    # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() 
+    server_socket = socket.create_server(("localhost", 6379)
+    
     connection, _ = server_socket.accept()
-    connection.sendall(b"+PONG\r\n")# wait for client
+    
+    with connection:
+        connection.recv(1024)
+        connection.send(pong.encode())
 
 
 if __name__ == "__main__":

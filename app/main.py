f7c27553b68d19e90ff4bbc40b3fe1f6bbc50bde
Sarthak Nayak <zarthak23n@gmail.com>
2025-08-16 02:11:07 +0530

    stage 3

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import socket
 import threading # noqa: F401
 
 
-def handle_client():
+def handle_client(connection):
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
     # Uncomment this to pass the first stage
